743bc97f9b4bb6e47a96d48034823beebe7f37dc
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-08-07 00:10:32 +0300

    It`s working!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,10 @@ use image_to_ascii::Image;
 use image_to_ascii::converter::Converter;
 
 fn main() {
-    let image = Image::load("/home/blinc/Загрузки/admin.jpg");
+    let image = Image::load("/");
 
     let size = image.get_size();
     println!("{}:{}", size.width, size.height);
 
-    Converter::convert(image);
+    Converter::convert(image, 3);
 }
--- src/converter.rs
+++ src/converter.rs
@@ -7,12 +7,19 @@ pub struct Converter {
 }
 
 impl Converter {
-    pub fn convert(image: Image) -> Self {
+    pub fn convert(image: Image, pixel_size: u32) -> Self {
         let size = image.get_size();
 
-        for x in 0..size.width/30 {
-            for y in 0..size.height/10 {
-                let pixel = image.get_pixel(x*30, y*10);
+        let char_width = pixel_size;
+        let char_height = pixel_size*2;
+
+
+        for y in 0..size.height / char_height {
+            for x in 0..size.width / char_width {
+                let pixel = image.get_pixel(
+                    (x * char_width).clamp(0, size.width-1),
+                    (y * char_height).clamp(0, size.height-1)
+                );
 
                 let c = pixel.average() / 32;
 
@@ -21,6 +28,7 @@ impl Converter {
 
             print!("\n");
         }
+
 
         Self {
             ascii: "".to_string()
