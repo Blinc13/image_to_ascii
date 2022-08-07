60b0fd456c95cd394b9dc4016c34f1a45ef29833
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-08-07 23:53:14 +0300

    Added OpenCL hello world kernel

--- src/converter.rs
+++ src/converter.rs
@@ -2,6 +2,7 @@ use crate::Image;
 use ocl::{
     Device,
     Context,
+    Program,
     Platform,
     Image as ClImage,
 };
@@ -10,11 +11,12 @@ const CHARS: &str = " -0=%$@#";
 
 pub struct Converter {
     device: Device,
-    context: Context
+    context: Context,
+    platform: Platform
 }
 
 impl Converter {
-    pub fn init() -> () {
+    pub fn init() -> Self {
         let platform = Platform::first().unwrap();
         println!("{}", platform);
         let device = Device::first(platform).unwrap();
@@ -23,8 +25,20 @@ impl Converter {
         let context = Context::builder()
             .platform(platform)
             .devices(device)
-            .build();
+            .build().unwrap();
+
+        let program = Program::builder()
+            .devices(device)
+            .source_file("src/opencl/kernel.cl")
+            .build(&context).unwrap();
 
         println!("{:?}", context);
+        println!("{:?}", program.to_string());
+
+        Self {
+            device,
+            context,
+            platform
+        }
     }
 }
---
+++ src/opencl/Kernel.cl
@@ -0,0 +1,4 @@
+kernel void hello()
+{
+	printf("Hello world!");
+}
