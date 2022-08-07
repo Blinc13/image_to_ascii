521bd272e19a68778f065bdd58d3decbbbd1d846
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-08-07 18:21:05 +0300

    Algorithm optimization

--- src/converter.rs
+++ src/converter.rs
@@ -21,17 +21,17 @@ pub fn convert<F>(image: Image, step: u32, pred: F) -> String
     let size = image.get_size();
 
     let width_step = step;
-    let height_step = step*2;
+    let height_step = step << 1;
 
 
     for y in 0..size.height / height_step {
         for x in 0..size.width / width_step {
             let pixel = image.get_pixel(
-                (x * width_step).clamp(0, size.width-1),
-                (y * height_step).clamp(0, size.height-1)
+                (x * width_step).clamp(0, size.width - 1),
+                (y * height_step).clamp(0, size.height - 1)
             );
 
-            let c = (pred(pixel.average()) / 32) as usize;
+            let c = (pred(pixel.average()) >> 5) as usize;
 
             output.push_str(&CHARS[c..c+1]);
         }
