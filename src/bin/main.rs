2358aedb09ca8b5702fcce71a62ba66b2497d5f2
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-08-07 23:42:06 +0300

    First developments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,36 +5,12 @@ use std::{
 use image_to_ascii::{
     Args,
     Image,
-    converter::{
-        default_convert,
-        convert_inverted
-    }
+    converter::Converter
 };
 use clap::Parser;
 
 fn main() {
-    let args = Args::parse();
-
-    if args.step == 0 {
-        print_and_exit("Incorrect step")
-    }
-
-    let image = match Image::load(&args.path) {
-        Ok(i) => i,
-        Err(e) => {
-            match e.kind() {
-                ErrorKind::NotFound => print_and_exit("File not found"),
-                ErrorKind::PermissionDenied => print_and_exit("You cannot read this file"),
-                ErrorKind::InvalidData => print_and_exit(&e.to_string()),
-                _ => panic!("Invalid error")
-            }
-        }
-    };
-
-    println!("{}", match args.inverted {
-        true => convert_inverted(image, args.step),
-        false => default_convert(image, args.step)
-    });
+    let conv = Converter::init();
 }
 
 fn print_and_exit(message: &str) -> ! {
--- src/converter.rs
+++ src/converter.rs
@@ -1,43 +1,30 @@
 use crate::Image;
+use ocl::{
+    Device,
+    Context,
+    Platform,
+    Image as ClImage,
+};
 
 const CHARS: &str = " -0=%$@#";
 
-
-pub fn default_convert(image: Image, step: u32) -> String {
-    convert(image, step, |x| x)
+pub struct Converter {
+    device: Device,
+    context: Context
 }
 
-pub fn convert_inverted(image: Image, step: u32) -> String {
-    convert(image, step, |x| u8::MAX - x)
-}
+impl Converter {
+    pub fn init() -> () {
+        let platform = Platform::first().unwrap();
+        println!("{}", platform);
+        let device = Device::first(platform).unwrap();
+        println!("{}", device);
 
+        let context = Context::builder()
+            .platform(platform)
+            .devices(device)
+            .build();
 
-pub fn convert<F>(image: Image, step: u32, pred: F) -> String
-    where F: Fn(u8) -> u8
-{
-    let mut output = String::new();
-
-
-    let size = image.get_size();
-
-    let width_step = step;
-    let height_step = step << 1;
-
-
-    for y in 0..size.height / height_step {
-        for x in 0..size.width / width_step {
-            let pixel = image.get_pixel(
-                (x * width_step).clamp(0, size.width - 1),
-                (y * height_step).clamp(0, size.height - 1)
-            );
-
-            let c = (pred(pixel.average()) >> 5) as usize;
-
-            output.push_str(&CHARS[c..c+1]);
-        }
-
-        output.push('\n');
+        println!("{:?}", context);
     }
-
-    output
 }
