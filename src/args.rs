7714bebfcbc35b60ff4c17a7b8acc43bb974a6d8
Blinc <65546512+Blinc13@users.noreply.github.com>
2022-10-13 16:37:27 +0300

    Rebuild args

--- src/args.rs
+++ src/args.rs
@@ -4,11 +4,10 @@ use clap::Parser;
 #[clap(about, version)]
 pub struct Args {
     /// Path to image
-    #[clap(short, long, value_parser)]
     pub path: String,
 
     /// Pixel step(the larger the lower the resolution)
-    #[clap(short, long, value_parser, default_value="10")]
+    #[clap(value_parser, default_value="10")]
     pub step: u32,
 
     /// Invert colors(if set, colors inverted)
